3ea977c5a8ee40d85aacfd7a646ae3543eb3d380
Brian Burkard <brianburkard@gmail.com>
2019-12-02 19:16:01 -0500

    save chats

--- mybot.js
+++ mybot.js
@@ -21,42 +21,74 @@ client.on("message", (message) => {
             "timestamp": message.createdTimestamp,
         }
 
-        console.log(logEntry);
-        let strEntry = JSON.stringify(logEntry);
-        log.log.push(strEntry);
-
-        fs.appendFile("./log.json", JSON.stringify(log), (err) => console.error);
-        //JSON.stringify(log)
+        //let entryJSON = JSON.stringify(logEntry);
+        readAndAppendLog(logEntry);
 
     } else if (message.content.startsWith(config.prefix + "prefix")) {
+
         //changes the prefix.
         let newPrefix = message.content.split(" ").slice(1, 2)[0];
         config.prefix = newPrefix;
         fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+
     } else if (message.content.startsWith(config.prefix + "who")) {
+
         message.channel.send("ID: " + message.author.id);
+
     } else if (message.content.startsWith(config.prefix + "sup")) {
+
         //Greeting
         if (message.author.id == config.owner) {
             message.channel.send("Hey " + message.author.username + "! You are amazing!");
         } else {
             message.channel.send("Hey there " + message.author.username + "!");
         }
-    } else if (message.content.startsWith(config.prefix + "stop") && message.author.id == config.owner) {
-        //Disconnects the Bot.
 
-            client.destroy();
-        } else if (message.content.startsWith(config.prefix + "quote")) {
-            
-            let name = message.content.split(" ") || null;
-            let quote = getRandomQuote(name);
-        }
+    } else if (message.content.startsWith(config.prefix + "stop") && message.author.id == config.owner) {
+        
+        //Disconnects the Bot.
+        client.destroy();
+
+    } else if (message.content.startsWith(config.prefix + "quote")) {
+
+        let name = message.content.split(" ").slice(1, 2)[0] || null;
+        let quote = getRandomQuote(name);
+    }
 
 });
 
 
-function getRandomQuote(){
+function getRandomQuote(name) {
+    fs.readFile('./log.json', function(err,data){
+        if(err){console.error(err);}
+        const parsedData = JSON.parse(data);
 
+        if(name){
+            const filteredByAuthor = parsedData.filter((entry)=>{
+                return entry.Author === name;
+            });
+            console.log(filteredByAuthor);
+        }else{
+            console.log(parsedData);
+        }
+    });
+}
+
+function readAndAppendLog(entry){
+    fs.readFile('./log.json', function(err,data){
+        if(err){console.error(err);}
+        // parse log
+        const parsedData = JSON.parse(data);
+
+        // append new data on to parsed log
+        parsedData.push(entry);
+
+        // stringify appended log.
+        const appendedLog = JSON.stringify(parsedData);
+
+        // Write to log file.
+        fs.writeFile("./log.json", appendedLog, (err) => console.error);
+    });
 }
 
 
